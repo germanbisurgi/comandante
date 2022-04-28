bb3a6d64271087793b13a47680de0853b446b7ef
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2022-04-28 13:26:49 +0200

    Hide devtool and updated commands

--- src/backend/index.js
+++ src/backend/index.js
@@ -22,7 +22,7 @@ const createWindow = () => {
     }
   })
   mainWindow.loadFile(path.join(__dirname, '..', 'frontend', 'index.html'))
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
